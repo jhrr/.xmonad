9ee2aaaafc87baeefc1770fd97ef75b877740816
jhrr <jhrr.daemon@gmail.com>
2013-07-28 19:27:42 +0200

    basic setup

---
+++ xmonad.hs
@@ -0,0 +1,19 @@
+import XMonad
+
+
+myTerminal          = "urxvt"
+myModMask           = mod4Mask
+myBorderWidth       = 3
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+myWorkspaces        = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+main = do
+  xmonad $ defaultConfig
+    { terminal          = myTerminal
+    , modMask           = myModMask
+    , borderWidth       = myBorderWidth
+    , focusFollowsMouse = myFocusFollowsMouse
+    , workspaces        = myWorkspaces
+    }
