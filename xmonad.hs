628e27fb4d903ecbe3604dcc4e892d5913d1df6e
jhrr <jhrr.daemon@gmail.com>
2013-08-02 21:27:20 +0200

    getting xmobar working, workspaces better now

--- xmonad.hs
+++ xmonad.hs
@@ -1,20 +1,33 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Actions.WorkspaceNames
+import XMonad.Util.Run(spawnPipe)
+import System.IO
 
 
 myTerminal          = "urxvt"
 myModMask           = mod4Mask
 myBorderWidth       = 2
+
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 
 myWorkspaces 	    = ["α", "β" ,"γ", "δ", "ε", "ζ", "η", "θ", "ι"]
+-- myWorkspaces    = ["一", "二", "三", "四", "五", "六", "七", "八", "九"]
 
-
-main = xmonad =<< xmobar defaultConfig
-    { terminal          = myTerminal
-    , modMask           = myModMask
-    , borderWidth       = myBorderWidth
-    , focusFollowsMouse = myFocusFollowsMouse
-    , workspaces        = myWorkspaces
-    }
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+	, layoutHook = avoidStruts $ layoutHook defaultConfig
+	, logHook    = dynamicLogWithPP xmobarPP
+	                   { ppOutput = hPutStrLn xmproc
+			   , ppTitle  = xmobarColor "green" "" . shorten 50
+			   }
+	, terminal          = myTerminal
+    	, modMask           = myModMask
+    	, borderWidth       = myBorderWidth
+    	, focusFollowsMouse = myFocusFollowsMouse
+    	, workspaces        = myWorkspaces 
+	}
