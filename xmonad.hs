d9360f0e3ef346554d2f445a55694cfa879ca8d8
jhrr <jhrr.daemon@gmail.com>
2013-08-16 22:32:41 +0200

    clean up

--- xmonad.hs
+++ xmonad.hs
@@ -14,23 +14,21 @@ myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 
 myWorkspaces 	    = ["α", "β" ,"γ", "δ", "ε", "ζ", "η", "θ", "ι"]
--- myWorkspaces             = ["一", "二", "三", "四", "五", "六", "七", "八", "九"]
--- myWorkspaces 	    = ["1", "2" ,"3", "4", "5", "6", "7", "8", "9"]
 
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
-	, layoutHook = avoidStruts $ layoutHook defaultConfig
-	, logHook    = dynamicLogWithPP xmobarPP
-	                   { ppOutput          = hPutStrLn xmproc
-			   , ppTitle  	       = xmobarColor "green" "" . shorten 100
-			   , ppHidden          = xmobarColor "white" ""
-			   , ppHiddenNoWindows = xmobarColor "gray" ""
-			   }
-	, terminal          = myTerminal
+	    , layoutHook = avoidStruts $ layoutHook defaultConfig
+	    , logHook    = dynamicLogWithPP xmobarPP
+	                       { ppOutput          = hPutStrLn xmproc
+			               , ppTitle  	       = xmobarColor "green" "" . shorten 100
+			               , ppHidden          = xmobarColor "white" ""
+			               , ppHiddenNoWindows = xmobarColor "gray" ""
+			               }
+        , terminal          = myTerminal
     	, modMask           = myModMask
     	, borderWidth       = myBorderWidth
     	, focusFollowsMouse = myFocusFollowsMouse
-    	, workspaces        = myWorkspaces 
+    	, workspaces        = myWorkspaces
 	}
