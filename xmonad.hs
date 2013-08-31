1861c3ed06a9797e0a9d68b58db9da8e251d5a3f
jhrr <jhrr.daemon@gmail.com>
2013-08-31 18:47:32 +0200

    sort out borked indentation

--- xmonad.hs
+++ xmonad.hs
@@ -7,28 +7,27 @@ import System.IO
 
 
 myTerminal          = "urxvt"
-myModMask           = mod4Mask
 myBorderWidth       = 2
-
+myModMask           = mod4Mask
+myWorkspaces 	    = ["α", "β" ,"γ", "δ", "ε", "ζ", "η", "θ", "ι"]
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 
-myWorkspaces 	    = ["α", "β" ,"γ", "δ", "ε", "ζ", "η", "θ", "ι"]
 
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
-	    , layoutHook = avoidStruts $ layoutHook defaultConfig
+            , layoutHook = avoidStruts $ layoutHook defaultConfig
 	    , logHook    = dynamicLogWithPP xmobarPP
 	                       { ppOutput          = hPutStrLn xmproc
-			               , ppTitle  	       = xmobarColor "green" "" . shorten 100
-			               , ppHidden          = xmobarColor "white" ""
-			               , ppHiddenNoWindows = xmobarColor "gray" ""
-			               }
-        , terminal          = myTerminal
-    	, modMask           = myModMask
-    	, borderWidth       = myBorderWidth
-    	, focusFollowsMouse = myFocusFollowsMouse
-    	, workspaces        = myWorkspaces
+			       , ppTitle  	   = xmobarColor "green" "" . shorten 100
+			       , ppHidden          = xmobarColor "white" ""
+			       , ppHiddenNoWindows = xmobarColor "gray" ""
+			       }
+            , terminal          = myTerminal
+            , modMask           = myModMask
+            , borderWidth       = myBorderWidth
+            , focusFollowsMouse = myFocusFollowsMouse
+            , workspaces        = myWorkspaces
 	}
