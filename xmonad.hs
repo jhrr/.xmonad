29e614ecf232af66202d7956990c18907ba9a383
jhrr <jhrr.daemon@gmail.com>
2013-12-12 01:35:29 +0100

    get first keybindings and window properties working

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,8 @@
 import XMonad
+import XMonad.Actions.WorkspaceNames()
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Actions.WorkspaceNames()
+import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Run(spawnPipe)
 import System.IO
 
@@ -12,11 +13,18 @@ myModMask           = mod4Mask
 myWorkspaces 	    = ["α", "β" ,"γ", "δ", "ε", "ζ", "η", "θ", "ι"]
 myFocusFollowsMouse = True
 
+myManageHook :: ManageHook
+myManageHook = composeAll . concat $
+   [ [ className =? "Firefox-bin" --> doShift "γ" ]
+   , [(className =? "Firefox" <&&> resource =? "Dialog") --> doFloat] ]
+
+myKeys :: [(String, X())]
+myKeys = [ ("M-g" , spawn "firefox") ]
 
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
+        { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         , logHook    = dynamicLogWithPP xmobarPP
                                { ppOutput          = hPutStrLn xmproc
@@ -29,4 +37,4 @@ main = do
         , borderWidth       = myBorderWidth
         , focusFollowsMouse = myFocusFollowsMouse
         , workspaces        = myWorkspaces
-	}
+	} `additionalKeysP` myKeys
